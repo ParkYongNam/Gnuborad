d6fdd46e46c8396eba5772874ffbd78a6cf33827
ParkYongNam <dkskcka3@naver.com>
2023-11-30 11:18:42 +0900

    그누보드

---
+++ html/navi.php
@@ -0,0 +1,19 @@
+ <ul>
+    <li>
+        <a href="./about.php">회사소개</a>
+        <ul>
+            <li><a href="./about.php">회사소게</a></li>
+            <li><a href="./about.php">오시는길</a></li>
+            <li><a href="./about.php">연혁</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="./contact.php">연락처</a>
+    </li>
+    <li>
+        <a href="./index.php">홈</a>
+    </li>
+    <li>
+        <a href="./index.php">깃</a>
+    </li>
+</ul> 
